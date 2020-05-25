d52e817037bc49fa72caef830d60c452f6f06298
@HiruniMalaviarachchi <mail2hiruni@gmail.com>
2020-05-25 15:00:10 +0530

    fixed issues - Wasana

--- Front-End-Client/src/App.js
+++ Front-End-Client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import "./App.css";
+//import "./App.css";
 import MainHomeLayout from "./components/main-home-components/MainHomeLayout";
-
+import Register from "./components/main-home-components/client-handle-components/Register";
+import EditUserProfile from "./components/main-home-components/client-handle-components/EditUserProfile"
 function App() {
   return (
     <div className="App">
 
-      <MainHomeLayout />
+      <EditUserProfile />
     
     </div>
   );
